138cc0b38f2b9c36a06aa000220f07866d4d360b
Your Name <you@example.com>
2021-06-30 14:48:25 +0200

    ex1

--- src/test/java/java8/ex01/Lambda_01_Test.java
+++ src/test/java/java8/ex01/Lambda_01_Test.java
@@ -40,7 +40,7 @@ public class Lambda_01_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO result ne doit contenir que des personnes adultes (age >= 18)
-        PersonPredicate test= (Person p)-> p.getAge() >=18 ;
+        PersonPredicate test=  p-> p.getAge() >=18 ;
 
 
 
@@ -62,7 +62,7 @@ public class Lambda_01_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO result ne doit contenir que des personnes dont le prénom est "first_10"
-        PersonPredicate test= (Person p)-> p.getFirstname().equals("first_10");
+        PersonPredicate test=  p-> p.getFirstname().equals("first_10");
 
 
         List<Person> result = filter(personList, test);
@@ -82,7 +82,7 @@ public class Lambda_01_Test {
         String passwordSha512Hex = "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff";
 
         // TODO result ne doit contenir que les personnes dont l'age est > 49 et dont le hash du mot de passe correspond à la valeur de la variable passwordSha512Hex
-        PersonPredicate test = (Person p)-> {
+        PersonPredicate test =  p-> {
                String mot = DigestUtils.sha512Hex(p.getPassword());
                if (mot.equals(passwordSha512Hex) ){
 
